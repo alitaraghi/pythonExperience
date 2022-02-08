61b5cffc530f8fad673ac5fe4068632c9e3e05c9
Ali Taraghi <alitaraghi@gmail.com>
2022-02-08 18:17:26 +0330

    upgrade code with random.choice func

--- pickRandomName.py
+++ pickRandomName.py
@@ -5,8 +5,10 @@ import random
 name_list=input("Please Enter a bunch of name and seprate them by camma','  :\n")
 name_list=name_list.split(",")
 #get total number of items in list
-count_number=len(name_list)
+#count_number=len(name_list)
 #pick a random number in list length range
-picked_name=name_list[random.randint(0,count_number-1)]
+#picked_name=name_list[random.randint(0,count_number-1)]
+#using choice function: quick way to pick an item randomly from list
+picked_name=random.choice(name_list)
 # show the name that piked randomly
 print("The list you Enter: \n",name_list,"The picked name is:",picked_name)
