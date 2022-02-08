6a924890f7a18f97ad1aa073af075faf13f7a87e
Ali Taraghi <alitaraghi@gmail.com>
2022-02-08 18:03:31 +0330

    complete commenting of code

--- pickRandomName.py
+++ pickRandomName.py
@@ -4,9 +4,9 @@ from random import random
 import random
 name_list=input("Please Enter a bunch of name and seprate them by camma','  :\n")
 name_list=name_list.split(",")
-#get total number of items
+#get total number of items in list
 count_number=len(name_list)
-#picka random number in list length range
+#pick a random number in list length range
 picked_name=name_list[random.randint(0,count_number-1)]
 # show the name that piked randomly
 print("The list you Enter: \n",name_list,"The picked name is:",picked_name)
