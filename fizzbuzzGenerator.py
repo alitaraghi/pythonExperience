dfbd163e52e192a9421d200dc720b80592c439b5
Ali Taraghi <alitaraghi@gmail.com>
2022-02-09 19:29:20 +0330

    add comment

--- fizzbuzzGenerator.py
+++ fizzbuzzGenerator.py
@@ -1,5 +1,5 @@
 end_number=int(input("Please tell me the finall number of game: "))
-#check if number divisible by 3 or 5 or both 3 and 5
+#check if number divisible by 3 or 5 or 3 and 5
 for number in range(1,end_number):
     if number%15==0:
         print("fizzbuzz")
