8e3be127406c030a955e3f42092fe581c7f6daeb
Ali Taraghi <alitaraghi@gmail.com>
2022-02-09 11:52:53 +0330

    correction of variable name

--- avergeUingForLoop.py
+++ avergeUingForLoop.py
@@ -1,10 +1,10 @@
 numbers=input("Input the list of number you want to find average!\n").split()
 total=0
-flag=0
+list_size=0
 #add all elements in list and count the size of list
 for number in numbers:
     total+=int(number)
-    flag+=1
+    list_size+=1
 #calculate average and round it by type casting    
-average=int(total /flag)   
+average=int(total /list_size)   
 print("The average of numbers is: "+str(average))    
